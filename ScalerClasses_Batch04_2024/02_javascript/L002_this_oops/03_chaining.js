d26021e1afb7db6fb9b0f3acc0af3f62eac3892e
Rajneesh <rajneeshkumar146@gmail.com>
2024-08-21 22:21:55 +0530

    update

---
+++ ScalerClasses_Batch04_2024/02_javascript/L002_this_oops/03_chaining.js
@@ -0,0 +1,26 @@
+// Statement 1.
+
+let ladder = {
+    step: 0,
+    up() {
+        this.step++;
+    },
+    down() {
+        this.step--;
+    },
+    showStep: function () {
+        console.log(this.step);
+    }
+};
+
+ladder.up();  // 1
+ladder.up(); // 2
+ladder.up(); // 3
+
+ladder.down(); // 2
+
+ladder.showStep(); // 2
+
+
+
+ladder.up().up().up().down().showStep();
