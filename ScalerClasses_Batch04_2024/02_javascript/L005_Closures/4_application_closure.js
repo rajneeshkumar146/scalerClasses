46e98b341d533ee7731c195a861a630000bc63bf
Rajneesh <rajneeshkumar146@gmail.com>
2024-08-29 22:56:02 +0530

    update

---
+++ ScalerClasses_Batch04_2024/02_javascript/L005_Closures/4_application_closure.js
@@ -0,0 +1,161 @@
+// Function Currying
+// function counter(args){
+//     let count = 0;
+//     count++;
+//     if(args === 0){
+//         return count;
+//     }else{
+//         return function inner(args){
+//             count++;
+//             if(args === 0){
+//                 return count;
+//             }else{
+//                 return inner;
+//             }
+//         }
+//     }
+// }
+
+// console.log(counter(0));  // 1
+// console.log(counter()(0)); // 2
+// console.log(counter()()(0)); // 3
+// console.log(counter()()()()()(0)); // 6
+
+// function sum(val){
+//     if(val === undefined){
+//         return 0;
+//     }else{
+//         let res = val;
+//         return function innerSum(val){
+//             if(val === undefined){
+//                 return res;
+//             }else{
+//                 res += val;
+//                 return innerSum;
+//             }
+//         }
+//     }
+// }
+
+function sum(a){
+    if(a === undefined){
+        return 0;
+    }
+
+    return function(b){
+        if(b != undefined){
+            return sum(a + b);
+        }
+        return a;
+    }
+}
+
+
+// console.log(sum()); // 0
+// console.log(sum(1)()); // 1
+// console.log(sum(3)(4)()); // 7
+// console.log(sum(3)(7)(8)()); // 18
+
+
+/**
+ * Memoization function
+ * creating private variables
+ * */
+
+/***************Private variables******/
+// function createEvenStack() {
+//     return {
+//         items:[],
+//         // push: function(val){}
+//         push(item) {
+//             if (item % 2 == 0) {
+//                 console.log("Is pushed: ", item);
+//                 this.items.push(item);
+//             }else {
+//                 console.log("Please input even value");
+//             }
+//         },
+//         // pop: function(){}
+//         pop() {  
+//             return this.items.pop();
+//         }
+//     };
+// }
+
+// function createEvenStack() {
+//     let items = [];
+//     return {
+//         // push: function(val){}
+//         push(item) {
+//             if (item % 2 == 0) {
+//                 console.log("Is pushed: ", item);
+//                 items.push(item);
+//             }else {
+//                 console.log("Please input even value");
+//             }
+//         },
+//         // pop: function(){}
+//         pop() {  
+//             return items.pop();
+//         }
+//     };
+// }
+
+// const stack = createEvenStack();
+// stack.push(10);
+// stack.push(12);
+// console.log("pop ele: ", stack.pop());
+// stack.push(20);
+// stack.push(30);
+// stack.push(40);
+// console.log(stack.items);
+// stack.items = [101, 1001, 10001]; // prevent this behaviour
+// console.log(stack.items);
+// console.log("pop ele: ", stack.pop());
+// console.log("pop ele: ", stack.pop());
+// console.log("pop ele: ", stack.pop());
+// console.log("pop ele: ", stack.pop());
+
+/**
+ * Memoization : sum calculation take a lot of time  with
+ * memoization we can store  result of costly calculations -> useMemo
+ * */
+
+function calc(n) {
+    let sum = 0;
+    for (let i = 0; i < n; i++) {
+        sum += n;
+    }
+    return sum;
+}
+
+// console.time();
+// let res = calc(1000000000);
+// console.log("res", res);
+// console.timeEnd();
+
+function memoize(cb){
+    let cache = {};
+    return function(val){
+        if(cache[val] != undefined){
+            return cache[val];
+        }
+
+        const res = cb(val);
+        cache[val] = res;
+        return res;
+    }
+}
+
+let efficientCalcFn = memoize(calc);
+console.time();
+let res2 = efficientCalcFn(1000000000);
+console.log("res", res2);
+console.timeEnd();
+
+console.time();
+let res3 = efficientCalcFn(1000000000);
+console.log("res", res3);
+console.timeEnd();
+
+
---
+++ ScalerClasses_Batch04_2024/02_javascript/L006_Objects/l002_object_creation.js
@@ -0,0 +1,150 @@
+/**
+ * ways to create object in js
+ * 1. object literal
+ * 2. Object.create 
+ * **/
+
+/******1. Object literal ****************
+ * 1. properties and methods
+ * 2. Object -> as your prototype (parent)
+ * ***/
+
+// let obj = {
+//     name: "Steve",
+//     address: {
+//         state: "Newyork",
+//         city: "Manhatten"
+//     },
+//     sayHi: function () {
+//         console.log(`${this.name} say's Hi`);
+//         console.log("object this: ", this);
+//         return this;
+//     }
+// };
+
+// console.log("Object: ", obj)
+// obj.sayHi();
+// console.log("Returend Value of Object This: ", obj.sayHi());
+
+/* Different data types and there parents. **/
+// console.log([1, 3, 2, 5, 4]);
+// console.log(obj);
+
+// let str = "Hi My Name is Rajneesh";  // Behne the scene: new String("Hi My Name is Rajneesh") -> return the result of 'primitiveValue'.
+// console.log(str);
+
+// console.log(new String("Hi My Name is Rajneesh"));
+// console.log(new Boolean(true));
+// console.log(new Number(2));
+
+/***************************Inbuilt object works ***************
+ * 
+ * 2. array -> Array -> Object -> null
+ * 3. function -> Function -> Object -> null
+ * 4. object -> Object ->  null
+ * 
+ * For Primitive : temeporary parents.
+ * a. num -> Number -> Object -> null
+ * b. str -> String -> Object -> null
+ * c. boolean -> Boolean -> Object -> null
+ * d. null and undefined have no parent
+ * 
+ * etc 
+*     whenever you want to access any method or property
+*      then that  primitive is typecased as a 
+*      children of there respective parent class and 
+*      then that method is applied on that non primitive 
+*      you are returned the answer. -> autoboxing
+*/
+
+/**********2. Object create ******/
+
+// This one have parent Object.
+// let obj1 = new Object();  
+// obj1.name = "Rajneesh";
+// console.log("obj1: ", obj1);
+
+/****1. you create an object without any parent**/
+// We need to use create and specificially convey that information that 
+// this object dosen't have any parent bu passing value null.
+// let obj2 = Object.create(null);  
+// obj2.fullName = "Rajneesh Kumar yadav";
+// console.log("obj2: ", obj2);
+
+/****2. Object with some differnet object as a parent.**/
+
+// let obj = {
+//     name: "Steve",
+//     address: {
+//         state: "Newyork",
+//         city: "Manhatten"
+//     },
+//     sayHi: function () {
+//         console.log(`${this.name} say's Hi`);
+//         console.log("object this: ", this);
+//         return this;
+//     }
+// };
+
+// let obj2 = Object.create(obj);
+// obj2.name = "Rajneesh";
+// obj2.lastName = "Kumar";
+// console.log("Obj2: ", obj2);
+
+/********3. creating an object from another object*/
+let obj = {
+    name: "Steve",
+    address: {
+        state: "Newyork",
+        city: "Manhatten"
+    },
+    sayHi: function () {
+        console.log(`${this.name} say's Hi`);
+    }
+};
+
+let obj2 = Object.create(obj);
+console.log(obj2);
+
+/**overriden the name prop*/
+obj2.name = "symphony";
+obj2.lastname = "rogers";
+console.log("1. ", obj2.name, " ", obj2.lastname, " ", obj2.address);
+console.log("2. ", obj.lastname);
+
+
+// console.log(obj.isPrototypeOf(obj2));   // true: is 'obj' parent of 'obj2'
+// console.log(obj2.isPrototypeOf(obj));   // false: is 'obj2' parent of 'obj'
+
+let obj3 = Object.create(obj2);
+obj3.friends = ["tony", "bruce"];
+obj3.fullName = "Rajneesh Kumar yadav";
+obj3.age = 26;
+console.log(obj3);
+
+//  loop -> object -> for in loop -> iterate over all the prop of the object , 
+// inherited which are enumrable to for in
+// for(let key in obj3){
+//     console.log("Keys are: ", key)
+// }
+
+// T: O(P + Q + R), where P is no of elements in obj3, Q is no of elements in obj2, R is no of elements in obj.
+// for(let key in obj3){
+//     if(obj3.hasOwnProperty(key)){
+//         console.log("My Keys are: ", key)
+//     }else if(obj2.hasOwnProperty(key)){
+//         console.log("My Parent Keys are: ", key)
+//     }else if(obj.hasOwnProperty(key)){
+//         console.log("My Grand Parent Keys are: ", key)
+//     }
+// }
+
+// Question: Can you write a loop to print all members of my object but you need to ignore member of all parent class and so on. And Can you optimize that operations.
+// T: O(P) where P is no of elements in obj3.
+
+for (let key in obj3) {
+    if (!obj3.hasOwnProperty(key)) {
+        break;
+    }
+    console.log("My Keys are: ", key);
+}
