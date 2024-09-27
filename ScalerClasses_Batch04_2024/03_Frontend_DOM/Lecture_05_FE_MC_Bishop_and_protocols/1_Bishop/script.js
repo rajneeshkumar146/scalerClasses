4ac19a90c5226046a97b73e2e8c0d4090f3a68d8
Rajneesh <rajneeshkumar146@gmail.com>
2024-09-27 22:32:31 +0530

    update

--- ScalerClasses_Batch04_2024/03_Frontend_DOM/Lecture_05_FE_MC_Bishop_and_protocols/1_Bishop/script.js
+++ ScalerClasses_Batch04_2024/03_Frontend_DOM/Lecture_05_FE_MC_Bishop_and_protocols/1_Bishop/script.js
@@ -1,6 +1,10 @@
+
+let YELLOW_COLOR = "yellow";
+
 window.addEventListener("load", function () {
     let table = this.document.querySelector("#table");
     let N = 8, M = 8;
+    player = "rook";
 
     // chess grid creation.
     // ri: row index, ci: column index
@@ -18,12 +22,12 @@ window.addEventListener("load", function () {
         table.appendChild(tr);
     }
 
-    hoverEffect(table, N, M);
+    hoverEffect(table, N, M, player);
 });
 
 
-function hoverEffect(table, N, M) {
-
+function hoverEffect(table, N, M, player) {
+    let boxArr = document.querySelectorAll(".box");
     table.addEventListener("mouseover", (event) => {
         let dataIndex = event.target.dataset.index;
         if (dataIndex === undefined) {
@@ -31,10 +35,66 @@ function hoverEffect(table, N, M) {
         }
         // console.log(dataIndex);
 
+        removeYellowColorFromAllCells(boxArr);
         let [curr_row, curr_col] = dataIndex.split("-").map(idx => idx);
-        
 
-    })
+        let directionVector = getDirectionVector(player);
+        storageOfPossibleMoves = possibleMoves(parseInt(curr_row), parseInt(curr_col), N, M, directionVector);
 
+        colorMyPossibleMoves(storageOfPossibleMoves, boxArr);
+    });
+
+    function removeYellowColorFromAllCells(boxArr) {
+        for (let boxCell of boxArr) {
+            boxCell.classList.remove(YELLOW_COLOR);
+        }
+    }
+}
+
+function possibleMoves(curr_row, curr_col, N, M, direction) {
+    storageOfPossibleMoves = {};
+
+    for (let dir of direction) {
+        for (let radius = 0; radius <= Math.max(N, M); radius++) {
+            let r = curr_row + (radius * dir[0]);
+            let c = curr_col + (radius * dir[1]);
+
+            if (r >= 0 && c >= 0 && r < N && c < M) {
+                let dataIndex = `${r}-${c}`;
+                storageOfPossibleMoves[dataIndex] = true;
+            } else {
+                break;
+            }
+        }
+    }
+
+    return storageOfPossibleMoves;
+}
+
+function colorMyPossibleMoves(storageOfPossibleMoves, boxArr) {
+    for (let boxCell of boxArr) {
+        let curr_dataIndex = boxCell.dataset.index;
+        if (storageOfPossibleMoves[curr_dataIndex]) {
+            boxCell.classList.add(YELLOW_COLOR);
+        }
+    }
+}
+
+function getDirectionVector(player) {
+    if (player === "bishop") {
+        return [[1, 1], [-1, 1], [1, -1], [-1, -1]];
+    } else if (player === "queen") {
+        return [[1, 1], [-1, 1], [1, -1], [-1, -1], [0, 1], [1, 0], [0, -1], [-1, 0]];
+    } else if (player === "rook") {
+        return [[0, 1], [1, 0], [0, -1], [-1, 0]];
+    }else if(player = "knight"){
+
+    }
+}
+
+function mouseLeave(table, boxArr) {
+    table.addEventListener("mouseleave", () => {
+        removeYellowColorFromAllCells(boxArr);
+    });
 }
 
