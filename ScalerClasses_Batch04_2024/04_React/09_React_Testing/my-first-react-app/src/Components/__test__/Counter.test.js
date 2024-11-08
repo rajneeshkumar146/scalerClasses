eda8a147d6197503f052128fd73fb3d1ddf40f85
Rajneesh <rajneeshkumar146@gmail.com>
2024-11-08 22:15:44 +0530

    update

--- ScalerClasses_Batch04_2024/04_React/09_React_Testing/my-first-react-app/src/Components/__test__/Counter.test.js
+++ ScalerClasses_Batch04_2024/04_React/09_React_Testing/my-first-react-app/src/Components/__test__/Counter.test.js
@@ -2,4 +2,15 @@ import { render, screen } from '@testing-library/react';
 
 describe("Counter component test cases", () => {
 
+    test("description of your test", () => {
+        // Logic of your test.
+
+        // Arrange your inputs.
+
+        // Action
+
+
+        // Assert
+    })
+
 });
